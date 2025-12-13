d55e8e60cef84e6abf1653b796352a8243b29605
SolalD. <solal.devil@gmail.com>
2025-12-13 14:53:48 +0100

    Ajout exception + Parser + Graph

--- src/Gedcom_Parsing/GedcomParser.java
+++ src/Gedcom_Parsing/GedcomParser.java
@@ -12,28 +12,3 @@ public class GedcomParser {
         graph = new GedcomGraph();
     }
 
-    public GedcomGraph parse(String filePath) throws IOException, GedcomNatException {
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            int lineNum = 0;
-
-            while ((line = reader.readLine()) != null) {
-                lineNum++;
-                line = line.trim();
-                if (line.isEmpty()) continue;
-
-                // 1. On découpe la ligne brute en morceaux utilisables
-                ParsedLine parsed = analyzeLine(line);
-
-                // 2. On traite selon le niveau (0, 1, 2...)
-                processLine(parsed);
-            }
-        }
-
-        // 3. Une fois la lecture finie, on lance la validation (Etape 2)
-        // C'est ici qu'on transforme les Strings en Objets et qu'on répare les erreurs
-        graph.construireEtValiderGraphe();
-
-        return graph;
-    }
-}
